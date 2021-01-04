77224ad79b95b5a50465fa8fe9adf5a78f5169b4
aroesec <aroe3sec@gmail.com>
2021-01-04 11:19:21 -0700

    added Profile Schema and middleware

---
+++ models/Profile.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongooose");
+const { mongo } = require("mongoose");
+const { stringify } = require("uuid");
+
+const ProfileScehma = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+      },
+      company: {
+        type: String,
+      },
+      location: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+      },
+      degree: {
+        type: String,
+      },
+      fieldofstudy: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+});
+
+module.exports = mongoose.model("profile", ProfileScehma);
