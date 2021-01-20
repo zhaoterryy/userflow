4f2df16f4fd396ae786829d142a70ebf94364b3f
aroesec <aroe3sec@gmail.com>
2021-01-20 09:20:19 -0700

    added uuid

---
+++ frontend/src/actions/alert.js
@@ -0,0 +1,9 @@
+import { SET_ALERT, REMOVE_ALERT } from './types';
+import uuid from 'uuid'
+//dispatch comes from THUNK middleware
+export const setAlert = (msg, alertType) => dispatch => {
+
+    
+
+
+}
--- frontend/src/reducers/alert.js
+++ frontend/src/reducers/alert.js
@@ -3,8 +3,12 @@ import { SET_ALERT, REMOVE_ALERT } from '../actions/types'
 const initialState = []; //alerts array
 
 export default function(state = initialState, action) {
-  switch(action.type){
-      case 'SET_ALERT'
-        return [...state, action.payload]
+  const { type, payload } = action;
+  switch(type){
+      case SET_ALERT:
+        return [...state, payload]
+      case REMOVE_ALERT:
+        return state.filter(alert => alert.id !== payload)
+
   }
 }
